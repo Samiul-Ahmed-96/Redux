d3e2426f6e82bdf6ba78e338a484ccc2df5619bf
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2022-08-16 14:26:39 +0600

    Updated

---
+++ react-redux/src/components/Counter/Counter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Counter = () => {
+    return (
+        <div className='counter-main'>
+            <h1>REACT-REDUX-COUNTER</h1>
+            <h4>Value</h4>
+        </div>
+    );
+};
+
+export default Counter;
--- redux-core/index.js
+++ redux-core/index.js
@@ -1,116 +1,213 @@
+// //State
+// //Dispatch action
+// //Reducer
+// //Store
+
+const { default: axios } = require("axios");
+const { createStore, applyMiddleware } = require("redux");
+const { default: thunk } = require("redux-thunk");
+
+// //Action type
+// const INCREMENT = "INCREMENT";
+// const DECREMENT = "DECREMENT";
+// const RESET = "RESET";
+// //Action with payload
+// const INCREMENT_BY_VALUE = "INCREMENT_BY_VALUE";
+// const DECREMENT_BY_VALUE = "DECREMENT_BY_VALUE";
+// const ADD_USER = "ADD_USER";
+// // const ADD_USER = "ADD_USER;
+
+// //State
+// const initialState = {
+//   count: 0,
+//   user: ["Anik"],
+// };
+
+// //Action  is a object (type , payload)
+
+// const incrementCounter = () => {
+//   return {
+//     type: INCREMENT,
+//   };
+// };
+// const decrementCounter = () => {
+//   return {
+//     type: DECREMENT,
+//   };
+// };
+
+// const resetCounter = () => {
+//   return {
+//     type: RESET,
+//   };
+// };
+// //Action with payload
+// const incrementByValue = (value) => {
+//   return {
+//     type: INCREMENT_BY_VALUE,
+//     payload: value,
+//   };
+// };
+// const decrementByValue = (value) => {
+//   return {
+//     type: DECREMENT_BY_VALUE,
+//     payload: value,
+//   };
+// };
+
+// const addUser = (value) => {
+//   return {
+//     type: ADD_USER,
+//     payload: value,
+//   };
+// };
+
+// //Reducer - is a pure function - build logic for action - used for update the state based on action
+
+// const counterReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case INCREMENT:
+//       return {
+//         ...state,
+//         count: state.count + 1,
+//       };
+//     case DECREMENT:
+//       return {
+//         ...state,
+//         count: state.count - 1,
+//       };
+
+//     case RESET:
+//       return {
+//         ...state,
+//         count: (state.count = 0),
+//       };
+//     //case with payload
+//     case INCREMENT_BY_VALUE:
+//       return {
+//         ...state,
+//         count: state.count + action.payload,
+//       };
+//     //case with payload
+//     case DECREMENT_BY_VALUE:
+//       return {
+//         ...state,
+//         count: state.count - action.payload,
+//       };
+
+//     case ADD_USER:
+//       return {
+//         ...state,
+//         user: [...state.user, action.payload]
+//       };
+//     default:
+//       state;
+//   }
+// };
+
+// //Store
+// //important 3 important method
+// // getState()
+// // dispatch()
+// // subscribe()
+
+// //Store
+// const store = createStore(counterReducer);
+
+// store.subscribe(() => {
+//   console.log(store.getState());
+// });
+
+// //Dispatch
+// store.dispatch(incrementCounter());
+// store.dispatch(incrementCounter());
+// store.dispatch(incrementCounter());
+// store.dispatch(incrementCounter());
+// store.dispatch(incrementCounter());
+// store.dispatch(decrementCounter());
+// store.dispatch(decrementCounter());
+// store.dispatch(resetCounter());
+// store.dispatch(incrementByValue(10));
+// store.dispatch(decrementByValue(5));
+// store.dispatch(addUser("Sakib"));
+// store.dispatch(addUser("Rakib"));
+
 //State
-//Dispatch action
+const initialTodosState = {
+  todos: [],
+  isLoading: false,
+  error: null,
+};
+//Constant Action
+const GET_TODO_REQUEST = "GET_TODO_REQUEST";
+const GET_TODO_FAILED = "GET_TODO_FAILED";
+const GET_TODO_SUCCESS = "GET_TODO_SUCCESS";
+//Action
+const getTodoRequest = () => {
+  return {
+    type: GET_TODO_REQUEST,
+  };
+};
+const getTodoFailed = (error) => {
+  return {
+    type: GET_TODO_FAILED,
+    payload: error,
+  };
+};
+const getTodoSuccess = (todos) => {
+  return {
+    type: GET_TODO_SUCCESS,
+    payload: todos,
+  };
+};
 //Reducer
-//Store
-
-const { createStore } = require("redux");
-
-//Action type
-const INCREMENT = "INCREMENT";
-const DECREMENT = "DECREMENT";
-const RESET = "RESET";
-//Action with payload
-const INCREMENT_BY_VALUE = "INCREMENT_BY_VALUE";
-const DECREMENT_BY_VALUE = "DECREMENT_BY_VALUE";
-// const ADD_USER = "ADD_USER;
-
-//State
-const initialCountState = {
-  count: 0,
-};
-
-const users = [{ name: "Anik" }];
-
-//Action  is a object (type , payload)
-
-const incrementCounter = () => {
-  return {
-    type: INCREMENT,
-  };
-};
-const decrementCounter = () => {
-  return {
-    type: DECREMENT,
-  };
-};
-
-const resetCounter = () => {
-  return {
-    type: RESET,
-  };
-};
-//Action with payload
-const incrementByValue = (value) => {
-  return {
-    type: INCREMENT_BY_VALUE,
-    payload: value,
-  };
-};
-const decrementByValue = (value) => {
-  return {
-    type: DECREMENT_BY_VALUE,
-    payload: value,
-  };
-};
-
-//Reducer - is a pure function - build logic for action - used for update the state based on action
-
-const counterReducer = (state = initialCountState, action) => {
+const todosReducer = (state = initialTodosState, action) => {
   switch (action.type) {
-    case INCREMENT:
+    case GET_TODO_REQUEST:
       return {
         ...state,
-        count: state.count + 1,
+        isLoading: true,
       };
-    case DECREMENT:
+    case GET_TODO_SUCCESS:
       return {
         ...state,
-        count: state.count - 1,
+        todos: action.payload,
+        isLoading: false,
       };
-
-    case RESET:
+    case GET_TODO_FAILED:
       return {
         ...state,
-        count: (state.count = 0),
-      };
-    //case with payload
-    case INCREMENT_BY_VALUE:
-      return {
-        ...state,
-        count: state.count + action.payload,
-      };
-    //case with payload
-    case DECREMENT_BY_VALUE:
-      return {
-        ...state,
-        count: state.count - action.payload,
+        isLoading: false,
+        payload: action.payload,
       };
     default:
-      state;
+      break;
   }
 };
 
-//Store
-//important 3 important method
-// getState()
-// dispatch()
-// subscribe()
+//Async function
+
+const fetchData = () => {
+  return (dispatch) => {
+    dispatch(getTodoRequest());
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        const todos = res.data;
+        const titles = todos.map(todo => todo.title)
+        dispatch(getTodoSuccess(titles))
+      })
+      .catch((err) =>{
+        dispatch(getTodoFailed(err))
+      });
+  };
+};
 
 //Store
-const store = createStore(counterReducer);
-
+const store = createStore(todosReducer, applyMiddleware(thunk));
 store.subscribe(() => {
   console.log(store.getState());
 });
 
-//Dispatch
-store.dispatch(incrementCounter());
-store.dispatch(incrementCounter());
-store.dispatch(incrementCounter());
-store.dispatch(incrementCounter());
-store.dispatch(incrementCounter());
-store.dispatch(decrementCounter());
-store.dispatch(decrementCounter());
-store.dispatch(resetCounter());
-store.dispatch(incrementByValue(10));
-store.dispatch(decrementByValue(5));
+//Dispatch  fetch data function
+store.dispatch(fetchData())
