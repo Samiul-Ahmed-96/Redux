36a72618b81ecd284700178bbdf543dc5345d72c
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2022-08-17 13:53:13 +0600

    Updated

--- react-redux-toolkit-crud/src/components/ShowBooks.js
+++ react-redux-toolkit-crud/src/components/ShowBooks.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteBook } from '../features/books/bookSlice';
 
 const ShowBooks = () => {
   const books = useSelector(state => state.bookReducer.books)
+  const dispatch = useDispatch();
+  const handleDelete = (id) =>{
+    dispatch(deleteBook(id))
+  }
   return (
     <div>
       { books &&
         books.map(singleBook => <div className='book-item'>
-            <h4>{singleBook?.id}</h4>
-            <h4>{singleBook?.title}</h4>
-            <h4>{singleBook?.author}</h4>
+            <h4>{singleBook.id}</h4>
+            <h4>{singleBook.title}</h4>
+            <h4>{singleBook.author}</h4>
+            <button onClick={()=>handleDelete(singleBook.id)}>Delete</button>
           </div> )
       }
     </div>
